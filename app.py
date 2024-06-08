019d2d031a8d4d91d4ac6225774edb84438a849a
GabrielGRR <gabrielguimaraesr.rocha@gmail.com>
2024-06-08 20:46:30 -0300

    add arquivos

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 import requests
 from flask import Flask, flash, redirect, render_template, request, session
-#teste
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
