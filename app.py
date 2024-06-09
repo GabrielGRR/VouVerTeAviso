5a0317124975834bfff8e72a3da48db7cd9f40a9
GabrielGRR <gabrielguimaraesr.rocha@gmail.com>
2024-06-09 00:19:53 -0300

    teste

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-import requests
+import datetime
+import urllib
+
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure application
+app = Flask(__name__)
 
