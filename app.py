6a9e61164c4fa4de4b19fd516e9ab6c1d1ad31af
GabrielGRR <gabrielguimaraesr.rocha@gmail.com>
2024-06-10 02:31:43 -0300

    add css e div

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import datetime
 import urllib
 
 from flask import Flask, flash, redirect, render_template, request, session
-from flask_session import Session
+#from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
 # Configure application
