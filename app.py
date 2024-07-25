f7f4726cc89c39e784ea7d2ed8aa7a73789b2e9a
GabrielGRR <gabrielguimaraesr.rocha@gmail.com>
2024-07-24 23:06:06 -0300

    botão adicionado dentro do forms, avaliar se é melhor refazer o forms ou o botão

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from calendar_logic import first_month, add_month, month_headers, month_belong_t
 import sqlite3 as sql
 
 
-from flask import Flask, flash, redirect, render_template, g, request
+from flask import Flask, flash, redirect, render_template, g, request, url_for 
 #from flask_session import Session
 #from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -27,6 +27,14 @@ num_rows = len(month_monthlist)
 def index():
     calendar_header = ['semana', 'D','S','T','Q','Q','S','S'] #ano tbm?
 
+    if request.method == "POST":
+        event_name = request.form.get("event_name")
+        hour_1 = request.form.get("hour_1")
+        hour_2 = request.form.get("hour_2")
+        print('eu tou aqui')
+
+        return f"Event name is :{event_name}\nevent starts at: {hour_1}\nevent ends at: {hour_2}"
+
     return render_template('index.html', 
                            num_rows= num_rows, 
                            calendar_header= calendar_header, 
@@ -35,6 +43,17 @@ def index():
                            excl_months = excl_months,
                            bool_list = bool_list,
                            month_of_the_day = month_of_the_day)
+
+@app.route('/layout', methods=["POST", "GET"])
+def layout():
+    if request.method == "POST":
+        print("POST")
+    event_name = request.form.get("event_name")
+    hour_1 = request.form.get("hour_1")
+    hour_2 = request.form.get("hour_2")
+
+    return f"Event name is :{event_name}\nevent starts at: {hour_1}\nevent ends at: {hour_2}"
+    #return render_template("layout.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
