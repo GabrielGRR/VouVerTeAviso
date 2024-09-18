89620f93110fa12b9378b3c96033043b635209d2
GabrielGRR <gabrielguimaraesr.rocha@gmail.com>
2024-09-18 10:55:16 -0300

    toggle de cor no botão de confirmar hora

--- static/event.js
+++ static/event.js
@@ -1,13 +1,18 @@
 console.log('event.js')
 
+function check_color(event){
+    const divClicada = event.target;
+    console.log(event);
+
+    if (divClicada.classList.contains('hour_selected')) {
+        divClicada.classList.remove('hour_selected');
+    } else {
+        divClicada.classList.add('hour_selected');
+    }
+}
+
 const box_divs = document.querySelectorAll('.min_box');
 
 box_divs.forEach(function(element) {
-    element.addEventListener("click", teste);
+    element.addEventListener("click", check_color);
 });
-
-function teste(event){
-    const divClicada = event.target;
-    divClicada.classList.add('hour_selected'); // Altere a cor de fundo para a cor desejada
-    console.log(event)
-}
