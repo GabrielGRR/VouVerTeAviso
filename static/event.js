e2e217fefc0e7d676c6e32642ceee7b92ece8bd1
GabrielGRR <gabrielguimaraesr.rocha@gmail.com>
2024-12-05 14:52:50 -0300

    simplificação JS

--- static/event.js
+++ static/event.js
@@ -1,18 +1,27 @@
 console.log('event.js')
 
-function check_color(event){
-    const divClicada = event.target;
-    console.log(event);
-
-    if (divClicada.classList.contains('hour_selected')) {
-        divClicada.classList.remove('hour_selected');
-    } else {
-        divClicada.classList.add('hour_selected');
-    }
-}
-
 const box_divs = document.querySelectorAll('.min_box');
 
 box_divs.forEach(function(element) {
-    element.addEventListener("click", check_color);
+    element.addEventListener("click", time_selected_toggle);
 });
+
+function time_selected_toggle(event){
+    const divClicada = event.target;
+    console.log(event);
+
+    const day = divClicada.getAttribute('day');
+    const month = divClicada.getAttribute('month');
+    const hour = divClicada.getAttribute('hour');
+    const min = divClicada.getAttribute('minute');
+
+    if (divClicada.getAttribute('selected') === 'true') {
+        divClicada.setAttribute('selected', 'false');
+        divClicada.classList.remove('hour_selected');
+
+    } else if (divClicada.getAttribute('selected') === 'false') {
+        divClicada.setAttribute('selected', 'true');
+        divClicada.classList.add('hour_selected');
+        console.log(day,month,hour,min);
+    }
+}
