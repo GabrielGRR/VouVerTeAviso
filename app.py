14a1e582673b5bf46486e026533f1245ebec1ce7
GabrielGRR <gabrielguimaraesr.rocha@gmail.com>
2024-12-05 22:31:18 -0300

    adicionado rota com o BD ao botão de eventos

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def process_data():
             return "Você tentou acessar diretamente pelo método GET"
         else:
             return "Falhou em alguma outra etapa"
-        
+
 @app.route('/<int:Id_event>', methods=["POST", "GET"])
 def events(Id_event):
     #cria conexão com o BD
@@ -112,6 +112,61 @@ def events(Id_event):
     connection.close() #talvez seja desnecessário
     print(result)
     return render_template('event.html', Id_event = Id_event, event_data = result )
+
+
+############## mexer neste código
+#Rota acessada para criar evento e processar dados
+@app.route('/user-data', methods=["POST", "GET"])
+def process_data():
+    if request.method == "POST":
+        connection = sql.connect('events_db.db')
+        cursor = connection.cursor() 
+        #coletando dados do JSON, o formato é em DICT{'days_array': [['24', 'Jul'], ['31', 'Jul']], 'event_min_hour': '4', 
+        # 'event_min_minute': '50', 'event_max_hour': '7', 'event_max_minute': '50', 'event_name': 'ssss'}
+        
+        data_package = request.get_json()
+        event_name = data_package.get('event_name')
+        execute_command = "INSERT INTO event(Event) VALUES(?)"
+        cursor.execute(execute_command, [event_name])
+        connection.commit()
+
+        #agora é adicionado outras informações, como horário mínimo e máximo e linkando ao ID único do evento
+        execute_command = """INSERT INTO event_days_hours(
+        Id_event, Day, Month, Event_min_hour, Event_min_minute, Event_max_hour, Event_max_minute
+        ) VALUES(?, ?, ?, ?, ?, ?, ?)"""
+
+        Id_event = cursor.lastrowid
+
+        days_array = data_package.get('days_array')
+
+        for i in range(len(days_array)):
+            Day = days_array[i][0]
+            Month = days_array[i][1]
+
+            #possivelmente terei de escalar este código ao incluir horários individuais dos dias de evento
+            
+            Event_min_hour = data_package.get('event_min_hour')
+            Event_min_minute = data_package.get('event_min_minute')
+            Event_max_hour = data_package.get('event_max_hour')
+            Event_max_minute = data_package.get('event_max_minute')
+            cursor.execute(execute_command, [Id_event, Day, Month, Event_min_hour, 
+                                            Event_min_minute, Event_max_hour, Event_max_minute])
+            connection.commit()
+
+        print(data_package)
+
+        connection.close()   
+        return jsonify({"url": f"/{Id_event}", 
+                        "Id_event": Id_event
+                        })
+
+    #aqui com certeza tem problema
+    else:
+        print("deu ruim")
+        if request.method == "GET":
+            return "Você tentou acessar diretamente pelo método GET"
+        else:
+            return "Falhou em alguma outra etapa"
 
 if __name__ == '__main__':
     app.run(debug=True)
