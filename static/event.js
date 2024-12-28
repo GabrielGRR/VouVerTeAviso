c420bba426487ad29dda74acf1d83e28824827ba
GabrielGRR <gabrielguimaraesr.rocha@gmail.com>
2024-12-28 16:59:14 -0300

    Voltando o JS antes de dar problema

--- static/event.js
+++ static/event.js
@@ -31,76 +31,23 @@ box_divs.forEach(function(element) {
 // Alterna entre estados
 function time_selected_toggle(event){
     const divClicada = event.target;
-    console.log(event);
-
-    // talvez chamar a função todas as vezes de populate_cor_Divs?
-    // se selected +1 user count e add name?
-
-    
-
-    // possivelmente desnecessário? mas é um bom sanity check
-    const day = divClicada.getAttribute('day');
-    const month = divClicada.getAttribute('month');
-    const hour = divClicada.getAttribute('hour');
-    const min = divClicada.getAttribute('minute');
 
     // remover da array
     if (divClicada.getAttribute('selected') === 'true') {
         divClicada.setAttribute('selected', 'false');
         divClicada.classList.remove('hour_selected');
-        users_dict[divClicada.id].pop();
-        
-        find_equivalent_div(divClicada,-1)
-
-        populate_colors(users_current_times);
-
-        //aqui vai ter que refazer a contagem de todos os max users, talvez repetir o código para o debaixo
 
     // adicionar à array
     } else if (divClicada.getAttribute('selected') === 'false') {
         divClicada.setAttribute('selected', 'true');
         divClicada.classList.add('hour_selected');
-        console.log(month,day,hour,min);
-        if (!users_dict[divClicada.id]){
-            users_dict[divClicada.id] = []
-        };
-        users_dict[divClicada.id].push(user_name);  
-
-        find_equivalent_div(divClicada,+1)
-
-        populate_colors(users_current_times);
     }
 };
-
-function find_equivalent_div(clicked_div, operator){
-    const day = clicked_div.getAttribute('day');
-    const month = clicked_div.getAttribute('month');
-    const hour = clicked_div.getAttribute('hour');
-    const min = clicked_div.getAttribute('minute');
-
-    const right_panel_divs = document.querySelectorAll('div[users_quantity]');
-
-    right_panel_divs.forEach(function(element) {
-        if (day === element.getAttribute('day') &&
-            month === element.getAttribute('month')&&
-            hour === element.getAttribute('hour')&&
-            min === element.getAttribute('min')){                
-                let users_current_total = Number(element.getAttribute('users_quantity'));
-                users_current_total+=operator;
-                users_current_total = users_current_total.toString();
-                element.setAttribute('users_quantity', users_current_total);
-            }
-
-    });
-
-};
-
 
 // Salva nome do usuario para futuro fetch de BD
 function get_user_name(event){
     event.preventDefault(); // Previne o comportamento padrão do botão
     user_name = document.getElementById("user_name").value
-    console.log(user_name)
 
     document.getElementById("inform_username").classList.add('hidden_left_panel');
     
@@ -124,7 +71,6 @@ function add_user_times_to_db(){
         }}
     );
     
-    console.log(user_available_times_array);
 
     // arquivo json
     var user_data = {
@@ -133,7 +79,6 @@ function add_user_times_to_db(){
         "user_times": user_available_times_array
     }
 
-    console.log(user_data)
 
     fetch('/user-data', {
         method: 'POST',
@@ -157,7 +102,6 @@ function add_user_times_to_db(){
 fetch(`/get_users-time?Id_event=${window.location.pathname.slice(1)}`)
                 .then(response => response.json())
                 .then(data => {
-                    // console.log(data);
                     users_current_times = data;
                     populate_colors(data)
                 })
@@ -178,13 +122,6 @@ function populate_colors(users_current_times){
         }
 
         users_current_times.forEach(item => {
-            // console.log(month,day,hour,min)
-            // console.log('---')
-            // console.log(item.User_month);
-            // console.log(item.User_day);
-            // console.log(item.User_hour);
-            // console.log(item.User_minute);
-            // console.log(item.User_name)
             
             // Compara o valor dos atributos com o BD
             if (month === item.User_month.toString() &&
@@ -205,13 +142,13 @@ function populate_colors(users_current_times){
                     console.log('username é:',item.User_name);
     
                     users_dict[element.id].push(item.User_name);
-                    console.log('dicionario esta como:',users_dict)    
+                    // console.log('dicionario esta como:',users_dict)    
             }
         });        
     });
     console.log("max users are:", max_users);
     percent = 100/max_users;
-    populate_colors_on_divs(max_users);
+    populate_colors_right_panel(max_users);
     create_color_divs_ruler(max_users);
 };
 
@@ -219,32 +156,28 @@ function populate_colors(users_current_times){
 // possivlmente terá um loop criando divs dentro do display flex da régua
 // deixar salvo a cor "máxima", ir aumentando percent
 
-function populate_colors_on_divs(max_users){
+function populate_colors_right_panel(max_users){
     const users_divs = document.querySelectorAll('div[users_quantity]');
     users_divs.forEach(function(element){
         let users_quantity = Number(element.getAttribute('users_quantity'))
         if (users_quantity > 0) {
             element.style.backgroundColor = original_color;
             let shade_amount = max_users-users_quantity;
-            // let local_percent = 100/max_users;
- 
 
             for(let i=0;i<shade_amount;i++){
                 shadeRGBColor_div(element, shade_amount);
             }
         }
     });
-    
-    console.log('teste',users_dict);
 };
 
 function create_color_divs_ruler(max_users){
     const ruler_divs = document.getElementById('container_regua_temp');
 
-    // Remove todos os elementos filhos do contêiner
-    while (ruler_divs.firstChild) {
-        ruler_divs.removeChild(ruler_divs.firstChild);
-    }
+    // // Remove todos os elementos filhos do container
+    // while (ruler_divs.firstChild) {
+    //     ruler_divs.removeChild(ruler_divs.firstChild);
+    // }
 
     for(let i=0;i<max_users+1;i++){
         var ruler_qtt = document.createElement("div");
@@ -273,8 +206,7 @@ function shadeRGBColor_div(div_element, difference) {
 
     let local_percent = percent * difference
     
-    // console.log("maxxx users are:", max_users);
-    // console.log(local_percent)
+
     var f = original_color.slice(4, -1).split(","), 
         t = local_percent < 0 ? 0 : 255, 
         p = local_percent < 0 ? local_percent * -1 : local_percent, 
@@ -287,5 +219,5 @@ function shadeRGBColor_div(div_element, difference) {
     var newB = Math.round((t - B) * (p / 100)) + B;
 
     div_element.style.backgroundColor = "rgb(" + newR + "," + newG + "," + newB + ")";
-    // console.log('local percent',local_percent)
+
 }
